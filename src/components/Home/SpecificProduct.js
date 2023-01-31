98ef21370bdb9950b28aa838848511e7ea9c0d76
Jan Irzyk <janirzyk333@gmail.com>
2023-01-31 23:00:30 +0100

    Work about specific product design

--- src/components/Home/SpecificProduct.js
+++ src/components/Home/SpecificProduct.js
@@ -1,5 +1,20 @@
 import React from "react";
+import classes from "./SpecificProduct.module.css";
+import Button from "../UI/Button";
+import jpg from "../../images/MOBILE/image-speaker-zx9.png";
 
 export default function SpecificProduct() {
-  return <div>dsadsa</div>;
+  return (
+    <div className={classes.specificProduct}>
+      <img src={jpg} alt="product"></img>
+      <div className={classes.description}>
+        <h3>ZX9 SPEAKER</h3>
+        <p>
+          Uprade to premium speakers that are phenomenally built to deliver
+          truly remarkable sound
+        </p>
+        <Button />
+      </div>
+    </div>
+  );
 }
